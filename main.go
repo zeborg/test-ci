64d5f70154733c1e0da72ed852e5991b9d3918c7
Abhinav Sinha <37282098+zeborg@users.noreply.github.com>
2022-05-02 19:14:42 +0530

    Create main.go

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bytes"
+	"os/exec"
+)
+
+func Shell(command string) (error, string, string) {
+	var stdout bytes.Buffer
+	var stderr bytes.Buffer
+	cmd := exec.Command("bash", "-c", command)
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+	err := cmd.Run()
+	return err, stdout.String(), stderr.String()
+}
+
+func main() {
+  err, out, errout := Shell("./clusterawsadm ami list")
+  if err != nil {
+    log.Printf("error: %v\n", err)
+  }
+  
+  fmt.Println("--- stdout ---")
+  fmt.Println(out)
+  fmt.Println("--- stderr ---")
+  fmt.Println(errout)
+}
