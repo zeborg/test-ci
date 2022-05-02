74cd3328a698d7972eb7c300dc7cb5fa0fc181ad
Abhinav Sinha <37282098+zeborg@users.noreply.github.com>
2022-05-02 19:23:44 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,30 +1,35 @@
 package main
 
 import (
-	"bytes"
-	"fmt"
-	"log"
-	"os/exec"
+  "bytes"
+  "fmt"
+  "os/exec"
 )
 
 func Shell(command string) (error, string, string) {
-	var stdout bytes.Buffer
-	var stderr bytes.Buffer
-	cmd := exec.Command("bash", "-c", command)
-	cmd.Stdout = &stdout
-	cmd.Stderr = &stderr
-	err := cmd.Run()
-	return err, stdout.String(), stderr.String()
+  var stdout bytes.Buffer
+  var stderr bytes.Buffer
+	
+  cmd := exec.Command("bash", "-c", command)
+  cmd.Stdout = &stdout
+  cmd.Stderr = &stderr
+  err := cmd.Run()
+	
+  return err, stdout.String(), stderr.String()
 }
 
 func main() {
-  err, out, errout := Shell("./clusterawsadm ami list")
-  if err != nil {
-    log.Printf("error: %v\n", err)
-  }
-  
-  fmt.Println("--- stdout ---")
-  fmt.Println(out)
-  fmt.Println("--- stderr ---")
-  fmt.Println(errout)
+	err, out, errout := Shell("./clusterawsadm ami list --kubernetes-version v1.24.0")
+	if err != nil {
+		fmt.Printf("error: %v\n", err)
+	}
+
+	fmt.Println("--- stdout ---")
+	if out != "" {
+		fmt.Println(out)
+	} else {
+		fmt.Println("info: v1.24.0 release not found")
+	}
+	fmt.Println("--- stderr ---")
+	fmt.Println(errout)
 }
