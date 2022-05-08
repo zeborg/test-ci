f08ae62faee051a9e9e388fe7315bf3ef191ec7f
Abhinav Sinha <37282098+zeborg@users.noreply.github.com>
2022-05-08 21:44:06 +0530

    Update main.go

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
   "bytes"
+  "fmt"
   "log"
   "os/exec"
+  "strings"
 )
 
 func Shell(command string) (string, string, error) {
@@ -19,23 +21,44 @@ func Shell(command string) (string, string, error) {
 }
 
 func main() {
+	v := "v1.24.0"
+	ami_regions := "us-east-1"
+	supportedOS := []string{"amazon-2"}
+	
 	stderr, stdout, err := Shell("./clusterawsadm ami list --kubernetes-version 1.24.0 --owner-id 570412231501")
+	log.Printf("Info: Building AMI for Kubernetes %s.", v)
+	
 	if err != nil {
 		log.Fatalf("ERROR: %v", err)
 	}
 	if stderr != "" {
 		log.Fatalf("STDERR: %v", stderr)
 	} else if stdout == "" {
-		stderr, stdout, err := Shell("cd image-builder/images/capi && PACKER_FLAGS=\"-var=ami_regions=us-east-1 -var=kubernetes_series=v1.24 -var=kubernetes_semver=v1.24.0 -var=kubernetes_rpm_version=1.24.0-0 -var=kubernetes_deb_version=1.24.0-00 \" make build-ami-amazon-2")
-		if err != nil {
-			log.Fatalf("ERROR: %v", err)
-		}
-		if stderr != "" {
-			log.Fatalf("STDERR: %v", stderr)
-		}
+		kubernetes_semver := v
+		kubernetes_rpm_version := strings.TrimPrefix(v, "v") + "-0"
+		kubernetes_deb_version := strings.TrimPrefix(v, "v") + "-00"
+		kubernetes_series := strings.Split(v, ".")[0] + "." + strings.Split(v, ".")[1]
 
-		log.Println("--- stdout ---")
-		log.Println(stdout)	
+		flagsK8s := fmt.Sprintf("-var=ami_regions=%s -var=kubernetes_series=%s -var=kubernetes_semver=%s -var=kubernetes_rpm_version=%s -var=kubernetes_deb_version=%s ", ami_regions, kubernetes_series, kubernetes_semver, kubernetes_rpm_version, kubernetes_deb_version)
+
+		for _, os := range supportedOS {
+			switch os {
+			case "amazon-2":
+				log.Println(fmt.Sprintf("Info: Building AMI for OS %s", os))
+				log.Println(fmt.Sprintf("Info: flags:  \"%s\"", flags))
+				
+				stderr, stdout, err := Shell("cd image-builder/images/capi && PACKER_FLAGS=\"-var=ami_regions=us-east-1 -var=kubernetes_series=v1.24 -var=kubernetes_semver=v1.24.0 -var=kubernetes_rpm_version=1.24.0-0 -var=kubernetes_deb_version=1.24.0-00 \" make build-ami-amazon-2")
+				if err != nil {
+					log.Fatalf("ERROR: %v", err)
+				}
+				if stderr != "" {
+					log.Fatalf("STDERR: %v", stderr)
+				}
+
+				log.Println("--- stdout ---")
+				log.Println(stdout)
+			}
+		}
 	} else {
 		log.Println("Info: AMI for Kubernetes v1.24.0 already exists. Skipping image building.")
 	}
